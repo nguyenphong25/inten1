4faef95933e042e6219023e9b8a6e1e433a316dd
nguyenphong25 <60138169+nguyenphong25@users.noreply.github.com>
2020-02-03 09:51:41 +0700

    Initial commit

---
+++ app/src/main/java/ntphong/example/myapplication/the2activity.kt
@@ -0,0 +1,12 @@
+package ntphong.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class the2activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_the2activity)
+    }
+}
